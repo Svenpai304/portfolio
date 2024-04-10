6d38d46f1d48c5219506e4922d241243dd089ed8
DrRon <DoctorRon64@users.noreply.github.com>
2024-04-10 22:03:10 +0200

    new page themes, working on contact page

---
+++ src/assets/js/app.js
@@ -0,0 +1,6 @@
+function sendEmail(message) {
+    var email = message.emailId;
+    var subject = message.subject;
+    var emailBody = 'Hi '+message.from;
+    document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody;
+}
