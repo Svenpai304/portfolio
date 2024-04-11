30662806497837ba50a87f54e6d11db5629268b4
Lefthandmedia <junkmail@lefthandmedia.com>
2024-04-11 13:43:06 +0200

    im age test add DOCS to gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -8,105 +8,107 @@ import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 import replace from 'gulp-replace';
 import browserSync from 'browser-sync';
-import imagemin, {gifsicle, mozjpeg, optipng, svgo} from 'gulp-imagemin';
+import imagemin, { gifsicle, mozjpeg, optipng, svgo } from 'gulp-imagemin';
 
-const {src, dest, watch, series, parallel} = gulp;
+const { src, dest, watch, series, parallel } = gulp;
 const browser = browserSync.create();
 const sass = gulpSass(dartSass);
 
 const files = {
-    scssPath: 'src/assets/scss/**/*.scss',
-    jsPath: 'src/assets/js/**/*.js',
-    htmlPath: 'src/html/*.html',
-    imgPath: 'src/assets/img/**/*.*',
+  scssPath: 'src/assets/scss/**/*.scss',
+  jsPath: 'src/assets/js/**/*.js',
+  htmlPath: 'src/html/*.html',
+  imgPath: 'src/assets/img/**/*.*',
 };
 
 function scssTask(cb) {
-    return src(files.scssPath, {sourcemaps: true})
-        .pipe(sass({outputStyle: 'compressed', includePaths: ['node_modules']}).on('error', sass.logError))
-        .pipe(postcss([autoprefixer()]))
-        .pipe(dest('docs/assets/css/', {sourcemaps: '.'}))
-        .on('end', cb);
+  return src(files.scssPath, { sourcemaps: true })
+    .pipe(sass({ outputStyle: 'compressed', includePaths: ['node_modules'] }).on('error', sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(dest('docs/assets/css/', { sourcemaps: '.' }))
+    .on('end', cb);
 }
 
 function jsTask(cb) {
-    return src(files.jsPath, {sourcemaps: true})
-        .pipe(concat('app.js'))
-        .pipe(terser())
-        .pipe(dest('docs/assets/js/', {sourcemaps: '.'}))
-        .on('end', cb);
+  return src(files.jsPath, { sourcemaps: true })
+    .pipe(concat('app.js'))
+    .pipe(terser())
+    .pipe(dest('docs/assets/js/', { sourcemaps: '.' }))
+    .on('end', cb);
 }
 
 function imgTask(cb) {
-    return src(files.imgPath)
-        .pipe(imagemin([
-            gifsicle({interlaced: true}),
-            mozjpeg({quality: 75, progressive: true}),
-            optipng({optimizationLevel: 5}),
-            svgo({
-                plugins: [
-                    {
-                        name: 'removeViewBox',
-                        active: true
-                    },
-                    {
-                        name: 'cleanupIDs',
-                        active: false
-                    }
-                ]
-            })
-        ]))
-        .on('error', console.error) // Handle errors
-        .pipe(dest('docs/assets/img/')) // Save optimized images
-        .on('end', cb);
+  return src(files.imgPath)
+    .pipe(imagemin([
+      gifsicle({ interlaced: true }),
+      mozjpeg({ quality: 75, progressive: true }),
+      optipng({ optimizationLevel: 5 }),
+      svgo({
+        plugins: [
+          {
+            name: 'removeViewBox',
+            active: true
+          },
+          {
+            name: 'cleanupIDs',
+            active: false
+          }
+        ]
+      })
+    ]))
+    .on('error', console.error) // Handle errors
+    .pipe(dest('docs/assets/img/')) // Save optimized images
+    .on('end', cb);
 }
 
+gulp.task('images', imgTask);
+
 function htmlTask(cb) {
-    return src(files.htmlPath)
-        .pipe(dest('docs/'))
-        .on('end', cb);
+  return src(files.htmlPath)
+    .pipe(dest('docs/'))
+    .on('end', cb);
 }
 
 function cacheBustTask(cb) {
-    var cbString = new Date().getTime();
-    return src(['src/html/index.html'])
-        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
-        .pipe(dest('docs/'))
-        .on('end', cb);
+  var cbString = new Date().getTime();
+  return src(['src/html/index.html'])
+    .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+    .pipe(dest('docs/'))
+    .on('end', cb);
 }
 
 function browserSyncServe(cb) {
-    browser.init({
-        server: {
-            baseDir: 'docs/',
-        },
-        notify: {
-            styles: {
-                top: 'auto',
-                bottom: '0',
-            },
-        },
-    });
-    cb();
+  browser.init({
+    server: {
+      baseDir: 'docs/',
+    },
+    notify: {
+      styles: {
+        top: 'auto',
+        bottom: '0',
+      },
+    },
+  });
+  cb();
 }
 
 function browserSyncReload() {
-    console.log('relolzxcad')
-    browser.reload();
+  console.log('relolzxcad')
+  browser.reload();
 }
 
 function watchTask(cb) {
-    watch(files.scssPath).on('all', gulp.series(scssTask, browserSyncReload));
-    watch(files.jsPath).on('all', gulp.series(jsTask, browserSyncReload));
-    watch(files.htmlPath).on('all', gulp.series(htmlTask, cacheBustTask, browserSyncReload));
-    //watch(files.imgPath).on('all', gulp.series(imgTask, browserSyncReload));
-    watch('docs/**/*.html').on('all', browserSyncReload);
-    cb();
+  watch(files.scssPath).on('all', gulp.series(scssTask, browserSyncReload));
+  watch(files.jsPath).on('all', gulp.series(jsTask, browserSyncReload));
+  watch(files.htmlPath).on('all', gulp.series(htmlTask, cacheBustTask, browserSyncReload));
+  watch(files.imgPath).on('all', gulp.series('images', browserSyncReload));
+  watch('docs/**/*.html').on('all', browserSyncReload);
+  cb();
 }
 
 export default series(
-    parallel(scssTask, jsTask, htmlTask),
-    cacheBustTask,
-    browserSyncServe,
-    watchTask
+  parallel(scssTask, jsTask, htmlTask),
+  cacheBustTask,
+  browserSyncServe,
+  watchTask
 );
