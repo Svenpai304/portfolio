9bd81e370e079387a6387e58969e2b0c23685295
DrRon <DoctorRon64@users.noreply.github.com>
2024-04-17 22:13:04 +0200

    projects background

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -20,11 +20,55 @@ window.addEventListener('scroll', function () {
   }
 });
 
+//move background
+document.addEventListener('DOMContentLoaded', function() {
+  const background = document.querySelector('.projBg');
+  const tileWidth = background.offsetWidth;
+  const tileHeight = background.offsetHeight;
 
-// adjustIframeHeight();
-// document.getElementById('iframeContent').addEventListener('load', adjustIframeHeight);
-// changeIframeSrc('Proj/code.html');
+  //offset
+  let positionX = 0;
+  let positionY = 0;
+  //direction
+  let directionX = -1;
+  let directionY = 1;
+  //speed
+  let speed = 0.1;
 
-// function initLightbox() {
-//   const lightbox = GLightbox({ ...options });
-// }
+  function moveBackground() {
+    positionX += speed * directionX;
+    positionY += speed * directionY;
+    background.style.backgroundPosition = positionX + 'px ' + positionY + 'px';
+
+    if (directionX === -1 && positionX <= -tileWidth) {
+      positionX = 0;
+    } else if (directionX === 1 && positionX >= tileWidth) {
+      positionX = 0;
+    }
+
+    if (directionY === -1 && positionY <= -tileHeight) {
+      positionY = 0;
+    } else if (directionY === 1 && positionY >= tileHeight) {
+      positionY = 0;
+    }
+
+    requestAnimationFrame(moveBackground);
+  }
+
+  moveBackground();
+});
+
+
+window.addEventListener('scroll', function() {
+  var button = document.querySelector('.scrollButton');
+  if (window.scrollY < 100) {
+    button.style.opacity = '0';
+  } else {
+    button.style.opacity = '1';
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var button = document.querySelector('.scrollButton');
+  button.style.opacity = '0';
+});
