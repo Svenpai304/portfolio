5128e96afbaae864a5c35a48c024ac29abb5c95a
Lefthandmedia <junkmail@lefthandmedia.com>
2024-04-17 20:47:59 +0200

    add lightbox to index.html

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,26 +1,30 @@
-function changeIframeSrc(src) {
-    document.getElementById('iframeContent').src = src;
-    document.querySelectorAll('.selectButton a').forEach(button => {
-        button.classList.remove('disabled');
-    });
-    document.getElementById(`${src.split('/')[1].split('.')[0]}Btn`).classList.add('disabled');
-}
+// function changeIframeSrc(src) {
+//   document.getElementById('iframeContent').src = src;
+//   document.querySelectorAll('.selectButton a').forEach(button => {
+//     button.classList.remove('disabled');
+//   });
+//   document.getElementById(`${src.split('/')[1].split('.')[0]}Btn`).classList.add('disabled');
+// }
 
-function adjustIframeHeight() {
-    var iframe = document.getElementById('iframeContent');
-    iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-}
+// function adjustIframeHeight() {
+//   var iframe = document.getElementById('iframeContent');
+//   iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+// }
 
-window.addEventListener('scroll', function() {
-    var button = document.querySelector('.scrollButton');
-    if (window.scrollY < 100) {
-        button.style.opacity = '0';
-    } else {
-        button.style.opacity = '1';
-    }
+window.addEventListener('scroll', function () {
+  var button = document.querySelector('.scrollButton');
+  if (window.scrollY < 100) {
+    button.style.opacity = '0';
+  } else {
+    button.style.opacity = '1';
+  }
 });
 
 
-adjustIframeHeight();
-document.getElementById('iframeContent').addEventListener('load', adjustIframeHeight);
-changeIframeSrc('Proj/code.html');
+// adjustIframeHeight();
+// document.getElementById('iframeContent').addEventListener('load', adjustIframeHeight);
+// changeIframeSrc('Proj/code.html');
+
+// function initLightbox() {
+//   const lightbox = GLightbox({ ...options });
+// }
